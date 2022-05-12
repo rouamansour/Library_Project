7e8a9f71918711163d629b0ebc132393100959c3
rouamansour <rouamansour.rm@gmail.com>
2022-05-12 20:32:48 +0100

    MVC Model-View-Controller

---
+++ app.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import auteurRouter from "./routes/auteur.route.js"
+import editeurRouter from "./routes/editeur.route.js";
+import specialiteRouter from "./routes/specialite.route.js";
+import livreRouter from "./routes/livre.route.js";
+import userRouter from "./routes/user.route.js"
+//import userRouter from "./routes/user.route.js";
+import clientRouter from "./routes/client.route.js";
+import commandeRouter from "./routes/commande.route.js";
+import lignecommandeRouter from "./routes/lignecommande.route.js"; 
+
+const app = express(); //instancier le Framework Express
+dotenv.config(); 
+app.use(express.json());
+app.use(cors());
+app.use('/api/livres', livreRouter);
+app.use('/api/users', userRouter);
+// Connexion à la base données
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log("Connexion à la base de données réussie");
+    }).catch(err => {
+        console.log('Impossible de se connecter à la base de données', err);
+        process.exit();
+    });
+app.use('/api/auteurs', auteurRouter);
+app.use('/api/editeurs', editeurRouter);
+app.use('/api/specialites', specialiteRouter);
+
+//app.use('/api/users', userRouter);
+app.use('/api/users', clientRouter);
+app.use('/api/commandes', commandeRouter);
+app.use('/api/commandes', lignecommandeRouter);
+
+app.get("/", (req, res) => {
+    res.send("Bibliothèque");
+});
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+});
+
